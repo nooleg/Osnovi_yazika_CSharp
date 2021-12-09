99d68ba5af6609a10ff6b32ebcc235c088c7c4ee
nooleg <45056778+nooleg@users.noreply.github.com>
2021-12-09 22:52:43 +0500

    Add files via upload

---
+++ 002/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Давайте посчитаем ИМТ вашего тела.");
+Console.Write("Какой у Вас вес в килограммах? ");
+int v= Convert.ToInt32(Console.ReadLine());
+Console.Write("Какой у Вас рост в сантиметрах? ");
+float r= Convert.ToInt32(Console.ReadLine())/100;
+Console.WriteLine($"Ваш ИМТ тела {v/(r*r)}");
+Console.ReadKey();
---
+++ 003/Program.cs
@@ -0,0 +1,11 @@
+﻿void Koor (string s)
+{
+    string[] svr = s.Split(",");
+    if (svr.Length <2) { return false; }
+
+}
+Console.WriteLine("Давайте вычислим растояние между двумя точками.");
+Console.Write("Введите координаты 1ой точки в формате x,y: ");
+
+Koor( Console.ReadLine());
+
